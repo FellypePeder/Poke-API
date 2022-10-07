75c6b06a127afcaccb65c502751b5a6bb81b1310
FellypePeder <fellypecosme@outlook.com>
2022-10-06 22:55:49 -0300

    POR ENQUANTO FUNCIONANDO TUDOOOOO

--- poke-api/src/Components/Card/PokeCard.jsx
+++ poke-api/src/Components/Card/PokeCard.jsx
@@ -5,40 +5,34 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 function PokeCard() {
 
-  const [nome, setNome] = useState("")
-  const [peso, setPeso] = useState(0)
-  const [altura, setAltura] = useState(0)
-  const [img, setImg] = useState("")
-  const [tipo, setTipo] = useState("");
+  const [pokemons, setPokemons] = useState([])
+
+  // const [tipo, setTipo] = useState("");
+  // const [nome, setNome] = useState("")
+  // const [peso, setPeso] = useState(0)
+  // const [altura, setAltura] = useState(0)
+  // const [img, setImg] = useState("")
 
   useEffect(() => {
-    api.get("pokemon/pikachu")
+    api.get("pokemon/")
     .then((res) => {
-      setNome(res.data.name)
-      setPeso(res.data.weight)
-      setAltura(res.data.height)
-      setImg(res.data.sprites.front_default);
-      setTipo(res.data.types[0].type.name);
-      console.log(res)
+      setPokemons(res.data.results)
     })
+    .catch((err) => console.log(err))
   })
 
   return (
-  <>
-    <div className="row">
-      <div className="col-12 col-md-3">
-        <div className='card-prod'>
-          <div className='text-center'>
-            <img src={img} alt=""/>
+    <div className="row"> 
+      {
+        pokemons.map((pokemon) => (
+          <div className="col-12 col-md-3">
+            <div className='text-center'>
+              <p></p>
+            </div>
           </div>
-          <p className='nome'>{nome[0].toUpperCase() + nome.substring(1)}</p>
-          <p className='peso'>{peso}</p>
-          <p className='altura'>{altura}</p>
-          <p className='tipo'>{tipo[0].toUpperCase() + tipo.substring(1)}</p>
-        </div>
-      </div>
+        ))
+      }
     </div>
-    </>
   );
 }
   
