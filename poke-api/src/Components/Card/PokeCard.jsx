89ef987d813e471a05810d42410b2d75f7bed8ba
FellypePeder <fellypecosme@outlook.com>
2022-11-08 23:42:30 -0300

    Começando a incrementar o modal

--- poke-api/src/Components/Card/PokeCard.jsx
+++ poke-api/src/Components/Card/PokeCard.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from "react";
 import api from "../../api";
 import Modal from "react-bootstrap/Modal";
+
+import { AiOutlineClose } from "react-icons/ai";
 import "bootstrap/dist/css/bootstrap.css";
 import "./style.scss";
 
 function PokeCard({ url }) {
   const [pokeInfo, setPokeInfo] = useState();
   const [modalVis, setModalVis] = useState(false);
-  
 
   let imgPoke =
     "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/";
@@ -19,6 +20,7 @@ function PokeCard({ url }) {
   useEffect(() => {
     api.get(url).then((res) => {
       setPokeInfo(res.data);
+      console.log(res.data);
     });
   }, [url]);
 
@@ -56,19 +58,26 @@ function PokeCard({ url }) {
               aria-labelledby="contained-modal-title-vcenter"
               centered
             >
-              <Modal.Header closeButton onClick={() => setModalVis(false)}>
+              <Modal.Header>
                 <Modal.Title id="contained-modal-title-vcenter">
-                  {
-                    letraMaiscula(pokeInfo.name) +
+                  {letraMaiscula(pokeInfo.name) +
                     ", tipo: " +
-                    letraMaiscula(pokeInfo.types[0].type.name) + " " + 
-                    letraMaiscula(pokeInfo.types.length == 2 ? pokeInfo.types[1].type.name : " ")
-                  }
+                    letraMaiscula(pokeInfo.types[0].type.name) +
+                    " " +
+                    letraMaiscula(
+                      pokeInfo.types.length == 2
+                        ? pokeInfo.types[1].type.name
+                        : " "
+                    )}
+                </Modal.Title>
+                <Modal.Title onClick={() => setModalVis(false)}>
+                  <AiOutlineClose className="icone-fechar" />
                 </Modal.Title>
               </Modal.Header>
               <Modal.Body>
                 <p>Altura: {pokeInfo.height / 10 + "m"}</p>
                 <p className="peso-poke">Peso: {pokeInfo.weight / 10 + "kg"}</p>
+                <p className="ataque-poke">{letraMaiscula(pokeInfo.stats[1].stat.name)+": "+pokeInfo.stats[0].base_stat}</p>
               </Modal.Body>
             </Modal>
           </div>
